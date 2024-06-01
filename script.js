177ad843b0e7480e8e58b0b4c8636d2615d79b9f
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-06-02 01:27:24 +0700

    9

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 document.getElementById('surveyForm').addEventListener('submit', function(event) {
     event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var dob = document.getElementById('dob').value;
+    var favoriteAlbum = document.getElementById('favoriteAlbum').value;
